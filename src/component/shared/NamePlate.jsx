38881d756a5d856aeda57360271b80cf13deac5b
Abu Syeed Abdullah <72955464+asapial@users.noreply.github.com>
2025-09-09 21:52:49 +0600

    feat: NamePlate is added

---
+++ src/component/shared/NamePlate.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NamePlate = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="relative text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 drop-shadow-lg animate-gradient">
+        InBrief
+      </h1>
+    </div>
+  );
+};
+
+export default NamePlate;
--- src/component/shared/NavBar.jsx
+++ src/component/shared/NavBar.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import NamePlate from './NamePlate';
 
 const NavBar = () => {
+
+  const menuList=<>
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+        </li>
+        <li><a>Item 3</a></li>
+  </>
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm">
@@ -12,32 +21,14 @@ const NavBar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+{menuList}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-2xl"><NamePlate></NamePlate></a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+{menuList}
     </ul>
   </div>
   <div className="navbar-end">
